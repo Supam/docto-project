d252f8410b4a394b9b4438667b613c795b9d4fab
Thibault Viennot <thibault.viennot@epita.fr>
2023-07-10 18:20:10 +0200

    feat(backend): added DTO validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,24 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
 
-    @ApiProperty({ required: true, nullable: false, type: String })
+    @ApiProperty({
+        required: true,
+        nullable: false,
+        type: String
+    })
+    @IsString()
+    @IsNotEmpty()
     username: string;
 
     @ApiProperty({ type: String })
+    @IsString()
+    @IsNotEmpty()
     email: string;
 
     @ApiProperty({ type: String })
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
