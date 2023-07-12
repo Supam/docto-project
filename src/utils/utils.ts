3fbad34441776e26f0c6e6577b0708eba2a29dd6
Thibault Viennot <thibault.viennot@epita.fr>
2023-07-12 03:24:31 +0200

    fix(tests): fixed tests and updated utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,9 @@
-import { InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { HttpException, InternalServerErrorException, Logger, NotFoundException } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
 
 
 
 export function prismaErrorHandler(prismaError: Prisma.PrismaClientKnownRequestError) {
-    console.log(prismaError)
     switch (prismaError.code) {
 
         case 'P2012':
@@ -13,8 +12,12 @@ export function prismaErrorHandler(prismaError: Prisma.PrismaClientKnownRequestE
         case 'P2025':
             throw new NotFoundException()
 
+        case 'P2002':
+            throw new HttpException("Pre-Condition Failed", 412)
+
         default:
-            throw new InternalServerErrorException()
+            Logger.debug(prismaError.code)
+            throw new InternalServerErrorException("Unknow Prisma Error")
     }
 
 }
