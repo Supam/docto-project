f8b7c4d05a52f8e356e7991e5ad21d01e0273ea5
Thibault Viennot <thibault.viennot@epita.fr>
2023-07-12 11:07:08 +0200

    fix(backend: users): fixed swagger annotations in entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -24,7 +24,7 @@ export class UserEntity implements User {
     @ApiProperty({ type: Number })
     id: number;
 
-    @ApiProperty()
+    @ApiProperty({ type: Number })
     username: string;
 
     @ApiProperty({ type: String })
